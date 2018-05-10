876cb4c1feadfa4ba79544cc0c0f82b062969f29
webdev604 <webdev.604@hotmail.com>
2018-05-10 04:56:05 +0300

    add MySQL database

---
+++ updateddb.sql
@@ -0,0 +1,60 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.5.5-10.1.21-MariaDB : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `test`;
+
+/*Table structure for table `referral` */
+
+DROP TABLE IF EXISTS `referral`;
+
+CREATE TABLE `referral` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `reward` int(5) unsigned DEFAULT '10',
+  `status` enum('1','0') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+/*Data for the table `referral` */
+
+insert  into `referral`(`id`,`user_id`,`sender_id`,`reward`,`status`,`created_at`) values (8,29,24,20,'0','2018-05-05 04:19:00'),(9,29,25,10,'0','2018-05-05 01:41:30');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `age` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `course` int(3) unsigned DEFAULT '0',
+  `referral` varchar(50) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`name`,`age`,`email`,`password`,`course`,`referral`,`created_at`) values (24,'Maddy','','','d41d8cd98f00b204e9800998ecf8427e',0,'','2018-05-05 01:41:30'),(25,'Danial','','','d41d8cd98f00b204e9800998ecf8427e',1,'','2018-05-05 01:41:43'),(29,'admin','22','admin@admin.com','a66abb5684c45962d887564f08346e8d',0,'http://example.com/admin','2018-05-05 02:07:31');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
